8477526e52ca8efedc313cfb3e2b818f8319ed63
santilondono <santiloo2002@gmail.com>
2021-11-08 18:12:56 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EntregaFinalHPII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "BIENVENIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Contraseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Usuario";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(145, 179);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(112, 32);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "INGRESAR";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(160, 116);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(183, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(160, 65);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(183, 20);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUser_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 222);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace EntregaFinalHPII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Login()
+        {
+            int ban = 0;
+            //Extraemos la informacion del registro y se guarda en variables
+            string user = txtUser.Text;
+            string password = txtPassword.Text;
+
+            //Creamos la conexion, el command y el datareader
+            SQLiteConnection conexion;
+            SQLiteCommand comand;
+            SQLiteDataReader datareader;
+
+            //Le asignamos la cadena de conexion
+            conexion = new SQLiteConnection("Data Source=dboEntregaFinal.db;Version=3");
+            conexion.Open();//Abrimos la conexion
+            comand = conexion.CreateCommand();
+
+            //Asignamos al command el query
+            comand.CommandText = "SELECT * FROM tblLogin";
+
+            datareader = comand.ExecuteReader();//Ejecutamos la consulta
+
+            //Mediante el datareader leemos los datos obtenidos por la consulta
+            while (datareader.Read())
+            {
+                if (datareader.GetString(1) == user && datareader.GetString(2) == password)//Evaluamos si la informacion ingresada y la de la base de datos es igual
+                {
+                    //Abrir pantalla principal
+                    ban++;//Marcamos la bandera como verdadera
+                }
+            }
+
+            if (ban == 0)//Si la bandera es igual a cero el usuario y/o contraseña estaran incorectos
+            {
+                MessageBox.Show("USUARIO Y/O CONTRASEÑA INCORRECTOS");
+            }
+
+            conexion.Close();//Cerramos la conexion
+        }
+
+        //Evento keypress para iniciar sesion con la tecla enter
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                Login();
+                btnIngresar.Focus();
+            }
+        }
+
+        //Evento al pulsar el boton ingresar
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        //Evento keypress para passar el cursor de usuario a contraseña con la tecla enter
+        private void txtUser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+    }
+}
