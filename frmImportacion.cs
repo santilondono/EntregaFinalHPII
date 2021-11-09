8bd2105ca163d43257152d102fae438f02b02f1e
Santiago Ramírez Valencia <santiago.ramirez796@pascualbravo.edu.co>
2021-11-08 19:40:12 -0500

    Formulario importación, creada.

---
+++ frmImportacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntregaFinalHPII
+{
+    public partial class frmImportacion : Form
+    {
+        public frmImportacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
